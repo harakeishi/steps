6ee9b12cd452722216149ffa36faaa48b63b25aa
harakeishi <hara.for.work@gmail.com>
2023-10-07 17:00:39 +0900

    stepをchain出来るように

--- steps.go
+++ steps.go
@@ -3,7 +3,7 @@ package steps
 import "fmt"
 
 type Flow struct {
-	Steps []Step // List of steps in the flow
+	Steps []*Step // List of steps in the flow
 }
 
 type ResultType int
@@ -16,51 +16,30 @@ const (
 )
 
 type Step struct {
-	StepName      string                                 // Name of the step
-	Description   string                                 // Description of the step
-	Run           func(interface{}) (interface{}, error) // Function to run the step
-	Retry         int                                    // Number of times to retry the step if it fails
-	Prerequisites []string                               // List of steps that must be run before this step
-	DependsOn     []string                               // List of steps that this step depends on
-	Outputs       interface{}                            // Output of the step
-	Inputs        interface{}                            // Input to the step
-	Result        ResultType                             // Result of the step
+	StepName    string                                 // Name of the step
+	Description string                                 // Description of the step
+	Run         func(interface{}) (interface{}, error) // Function to run the step
+	Retry       int                                    // Number of times to retry the step if it fails
+	Outputs     interface{}                            // Output of the step
+	Inputs      interface{}                            // Input to the step
+	Result      ResultType                             // Result of the step
 }
 
 // RunStep runs the step
 func (s *Step) RunStep() error {
-	if !s.Check() {
-		s.Result = Skipped
-		return nil
-	}
 	outputs, err := s.Run(s.Inputs)
 	if err != nil {
-		// TODO: retry処理に関しては後で実装
+		s.Result = Failure
 		return err
 	}
+
 	s.Outputs = outputs
 	s.Result = Success
 	return nil
 }
 
-// GetStepName returns the name of the step
-func (s Step) GetStepName() string {
-	return s.StepName
-}
-
-// GetDescription returns the description of the step
-func (s *Step) SetRun(run func(interface{}) (interface{}, error)) {
-	s.Run = run
-}
-
-// TODO: あとで実装
-func (s Step) Check() bool {
-	// 実行にあたり前提条件が満たされているかチェックする
-	return true
-}
-
-func NewStep(stepName string, description string, run func(interface{}) (interface{}, error), retry int) *Step {
-	return &Step{
+func NewStep(stepName string, description string, run func(interface{}) (interface{}, error), retry int) Step {
+	return Step{
 		StepName:    stepName,
 		Description: description,
 		Run:         run,
@@ -68,30 +47,28 @@ func NewStep(stepName string, description string, run func(interface{}) (interfa
 	}
 }
 
-// NewFlow creates a new flow
-func (f *Flow) AddStep(step Step) {
-	f.Steps = append(f.Steps, step)
+// 前のstepの結果を次のstepの入力にする
+func (s *Step) SetInputs(inputs interface{}) {
+	s.Inputs = inputs
 }
 
-// Plot prints the flow
-func (f Flow) Plot() {
-	for i, step := range f.Steps {
-		fmt.Println(i, ":", step.StepName)
-		fmt.Println("  name:", step.StepName)
-		fmt.Println("  Description:", step.Description)
-		fmt.Println("  Retry:", step.Retry)
-		fmt.Println("  Prerequisites:", step.Prerequisites)
-		fmt.Println("  DependsOn:", step.DependsOn)
-		fmt.Printf("  Outputs:%#v\n", step.Outputs)
-		fmt.Printf("  Inputs:%#v\n", step.Inputs)
-	}
+// NewFlow creates a new flow
+func (f *Flow) AddStep(step Step) {
+	f.Steps = append(f.Steps, &step)
 }
 
 // Run runs the flow
 func (f Flow) Run() {
-	for _, step := range f.Steps {
+	for i, step := range f.Steps {
+		if i != 0 {
+			step.SetInputs(f.Steps[i-1].Outputs)
+		}
 		if step.RunStep() != nil {
 			fmt.Println("Error")
 		}
 	}
+}
+
+func NewFlow() *Flow {
+	return &Flow{}
 }
---
+++ test/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	steps "github.com/harakeishi/steps"
+)
+
+type TargetData struct {
+	TargetName string
+	TargetAge  int
+}
+
+func main() {
+	// 新規フローを作成
+	flow := steps.NewFlow()
+	// ステップを追加
+	flow.AddStep(
+		steps.NewStep(
+			"Get Target Data",
+			"処理の対象者を取得する",
+			func(inputs interface{}) (interface{}, error) {
+				// 処理の対象者を取得する処理
+				targetData := []TargetData{
+					{
+						TargetName: "Taro",
+						TargetAge:  20,
+					},
+					{
+						TargetName: "Jiro",
+						TargetAge:  30,
+					},
+				}
+				return targetData, nil
+			},
+			0,
+		))
+	flow.AddStep(
+		steps.NewStep(
+			"Print Target Data",
+			"処理の対象者を表示する",
+			func(inputs interface{}) (interface{}, error) {
+				// 処理の対象者を表示する処理
+				targetData := inputs.([]TargetData)
+				for _, data := range targetData {
+					println(data.TargetName)
+					println(data.TargetAge)
+				}
+				return nil, nil
+			},
+			0,
+		))
+	// フローを実行
+	flow.Run()
+}
